deebc4229a4cc3a3913cd239c4bc27e98617e4e8
Dmitro Loshik <dmitroloshik@gmail.com>
2023-04-03 18:49:02 +0300

    change burger function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // burger
 $(document).ready(function () {
     $(".burger").on("click", function (event) {
-        $(".burger, .nav__item").toggleClass("active");
-        $("body").toggleClass("lock");
+        $(".burger, .nav__item").addClass("active");
+        $("body").addClass("lock");
+    })
+    $(".list__item-link").on("click", function () {
+        $(".burger, .nav__item").removeClass("active");
+        $("body").removeClass("lock");
     })
 });
 
